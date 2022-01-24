225a5a1694bb883f9494996354877ec38be1d32c
Ryan Basile <91210643+s1324902@users.noreply.github.com>
2022-01-24 12:46:57 -0500

    My Info Version 1

---
+++ myInfo.py
@@ -0,0 +1,7 @@
+#CS175l-01
+#Ryan Basile
+#myInfo
+
+print ("Hello my name is Ryan Basile")
+print ("My address is 21 Meredith way Hazlet, New Jersey 07730")
+print ("My college major is software engineering")
