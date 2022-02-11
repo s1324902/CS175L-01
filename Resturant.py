5e87610720724c26ced9d74fd909cd8c00316e4d
Ryan Basile <91210643+s1324902@users.noreply.github.com>
2022-02-11 17:13:52 -0500

    Add files via upload

---
+++ Resturant.py
@@ -0,0 +1,39 @@
+#Ryan Basile
+#CS-175L-01
+#Resturant Assignment
+
+vegetarian='False'
+vegan='False'
+gluten_free='False'
+
+response1=(input('Is anyone in your party a vegetarian?'))
+if response1=='yes' or response1=='no':
+    vegetarian=True
+response2=(input('Is anyone in your party a vegan?'))
+if response2=='yes' or response2=='no':
+    vegan=True
+response3=(input('Is anyone in your party gluten free?'))
+if response3=='yes' or response3=='no':
+    gluten_free=True
+(print('Here are your resturant choices.'))
+
+if response2=='yes':
+    if response3=='yes' or response3=='no':
+        print('Corner Cafe\n'+\
+              "The Chef's Kitchen\n")
+else:
+    if response3=='yes':
+        print('Main Street Pizza Company\n'+\
+              'Corner Cafe\n'+\
+              "The Chef's Kitchen\n")
+    else:
+        print("Joe's Gourmet Burgers\n"+\
+              'Main Street Pizza Company/n'+\
+              'Corner Cafe\n'+\
+              "Mama's Fine Italian\n"+\
+              "The Chef's Kitchen\n")
+                
+
+
+          
+      
