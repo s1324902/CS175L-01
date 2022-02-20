8a6d86f8fce39257899c409307e67643ce9ba05b
Ryan Basile <91210643+s1324902@users.noreply.github.com>
2022-02-20 18:33:09 -0500

    Add files via upload

--- Resturant.py
+++ Resturant.py
@@ -5,33 +5,37 @@
 vegetarian='False'
 vegan='False'
 gluten_free='False'
+keep_going='yes'
 
-response1=(input('Is anyone in your party a vegetarian?'))
-if response1=='yes' or response1=='no':
-    vegetarian=True
-response2=(input('Is anyone in your party a vegan?'))
-if response2=='yes' or response2=='no':
-    vegan=True
-response3=(input('Is anyone in your party gluten free?'))
-if response3=='yes' or response3=='no':
-    gluten_free=True
-(print('Here are your resturant choices.'))
-
-if response2=='yes':
+while keep_going == 'yes':
+    response1=(input('Is anyone in your party a vegetarian?'))
+    if response1=='yes' or response1=='no':
+        vegetarian=True
+    response2=(input('Is anyone in your party a vegan?'))
+    if response2=='yes' or response2=='no':
+        vegan=True
+    response3=(input('Is anyone in your party gluten free?'))
     if response3=='yes' or response3=='no':
-        print('Corner Cafe\n'+\
-              "The Chef's Kitchen\n")
-else:
-    if response3=='yes':
-        print('Main Street Pizza Company\n'+\
-              'Corner Cafe\n'+\
-              "The Chef's Kitchen\n")
+        gluten_free=True
+    (print('Here are your resturant choices.'))
+
+    if response2=='yes':
+        if response3=='yes' or response3=='no':
+            print('Corner Cafe\n'+\
+                  "The Chef's Kitchen\n")
     else:
-        print("Joe's Gourmet Burgers\n"+\
-              'Main Street Pizza Company/n'+\
-              'Corner Cafe\n'+\
-              "Mama's Fine Italian\n"+\
-              "The Chef's Kitchen\n")
+        if response3=='yes':
+            print('Main Street Pizza Company\n'+\
+                  'Corner Cafe\n'+\
+                  "The Chef's Kitchen\n")
+        else:
+            print("Joe's Gourmet Burgers\n"+\
+                  'Main Street Pizza Company/n'+\
+                  'Corner Cafe\n'+\
+                  "Mama's Fine Italian\n"+\
+                  "The Chef's Kitchen\n")
+            keep_going=input('Would you like to do another resturant search?(Enter y for yes)')
+     
                 
 
 
